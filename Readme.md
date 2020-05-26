2da41d7fc06ff690970b08655ec98f9b60e800d6
siddardhak <imsiddardha@gmail.com>
2020-05-26 20:41:16 +1000

    added Readme

---
+++ Readme.md
@@ -0,0 +1,16 @@
+### Book Store Using Node.js and Sequelize
+
+# To start program 
+
+```
+clone repo
+```
+
+```
+npm install
+```
+
+```
+npm start
+```
+## Note you need to install MySQL to connect this program
--- util/databaseConnection.js
+++ util/databaseConnection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'Siddhu@1995', { dialect: 'mysql', host: 'localhost' })
+const sequelize = new Sequelize('node-complete', 'root', 'XXXXXX', { dialect: 'mysql', host: 'localhost' })
 
 
 module.exports = sequelize;
